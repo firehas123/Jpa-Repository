5b2574c72c65218e3c179f60997b31b0cf29b159
Muhammad Hassan Chattha <mrhassanch@gmail.com>
2023-07-17 00:05:23 +0500

    incorrect state;

--- src/main/java/com/example/demo/JpaConfig.java
+++ src/main/java/com/example/demo/JpaConfig.java
@@ -1,10 +1,10 @@
-package com.example.demo;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "com.example.demo")
-public class JpaConfig {
-    // Other configuration code
-}
+//package com.example.demo;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//
+//@Configuration
+//@EnableJpaRepositories(basePackages = "com.example.demo")
+//public class JpaConfig {
+//    // Other configuration code
+//}
